8b022cd8ece66f68b8fcc9c126eb0760c2874746
Ming Jing <jingming.cn@gmail.com>
2025-12-12 08:51:44 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -19,7 +19,7 @@
     <div class="branding d-flex align-items-cente">
 
       <div class="container position-relative d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
+        <a href="index.php" class="logo d-flex align-items-center">
           <!-- Uncomment the line below if you also wish to use an image logo -->
           <!-- <img src="assets/img/logo.webp" alt=""> -->
           <h1 class="sitename">PPI</h1>
