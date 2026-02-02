9c229815bcdaaf7aca5b39328d35ce40a981c57b
Ming Jing <jingming.cn@gmail.com>
2026-02-02 11:11:32 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -69,6 +69,15 @@
 
   <script src="https://lf-cdn.coze.cn/obj/unpkg/flow-platform/chat-app-sdk/1.2.0-beta.19/libs/cn/index.js"></script>
 <script>
+function getCookieByName(name) {
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) {
+        return match[2];
+    }
+    return null;
+}
+token_id = getCookieByName('PHPSESSID');
+
   new CozeWebSDK.WebChatClient({
     config: {
       bot_id: '7602086574654455860',
